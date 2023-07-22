4ece36459ad4311bc5652306da9ea18f783db773
cmtorresa <cesarmta87@gmail.com>
2023-07-22 07:36:33 -0500

    Creación de vistas parciales en el controlador

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -13,13 +13,33 @@ namespace APS_proyecto.Controllers
     {
         public IActionResult Index()
         {
-            var asignatura = new Asignatura 
+            return View( new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            });
+        }
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>()
             {
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre= " Programación"
+                            new Asignatura{Nombre="Matemáticas",
+                                UniqueId= Guid.NewGuid().ToString()
+                            } ,
+                            new Asignatura{Nombre="Educación Física",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Castellano",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Ciencias Naturales",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                            ,
+                            new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
 
             };
-            return View(asignatura);
+            return View("Multiasignatura", listaAsignaturas);
         }
     }
 }
